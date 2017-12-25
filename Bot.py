ab3d223795106525af60c1764aee9aadf5d00fd3
Brenno Barboza <brennol.c.b@hotmail.com>
2017-12-25 14:33:13 -0200

    Add ETH and BCH coins

--- Bot.py
+++ Bot.py
@@ -1,25 +1,42 @@
 import discord
 from discord.ext import commands 
 import requests
+from CriptoSearch import CriptoCurrency
+import asyncio
 
-client = commands.Bot(command_prefix='!', description='''Developed by: @DwarfThief#3805''', pm_help = True)
+bot = commands.Bot(command_prefix='!', description='''Developed by: @DwarfThief#3805''', pm_help = True)
 
-BOT_USER_TOKEN = " " #<- Put here your token
+BOT_USER_TOKEN = "Mzk0MzMxNzQwODM2OTg2ODgw.DSC23A.gk7Zje-wFWLPJq3OtvmUzJl51Vc"
 
-@client.event
+@bot.event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('------')
 
-@client.command()
-async def coin(compare1 : str, compare2 : str):
-    url = 'https://min-api.cryptocompare.com/data/price?fsym={compare1}&tsyms={compare2}'
+@bot.command()
+async def btc(currency : str):
+    url = 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=' + currency
     response = requests.get(url)
     canned = response.json()
-    await client.say(str(canned[compare2]))
+    await bot.say("1 BTC = " + str(canned[currency]) + " " + currency)
+    print("1 BTC = " + str(canned[currency]) + " " + currency)
 
+@bot.command()
+async def eth(currency : str):
+    url = 'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=' + currency
+    response = requests.get(url)
+    canned = response.json()
+    await bot.say("1 ETH = " + str(canned[currency]) + " " + currency)
+    print("1 ETH = " + str(canned[currency]) + " " + currency)
+    
+@bot.command()
+async def bch(currency : str):
+    url = 'https://min-api.cryptocompare.com/data/price?fsym=BCH&tsyms=' + currency
+    response = requests.get(url)
+    canned = response.json()
+    await bot.say("1 BCH = " + str(canned[currency]) + " " + currency)
+    print("1 BCH = " + str(canned[currency]) + " " + currency)
 
-
-client.run(BOT_USER_TOKEN)
+bot.run(BOT_USER_TOKEN)
